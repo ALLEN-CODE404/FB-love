5bedb921d87bb599dfa5509376080cb77a124265
Allen kurah <100374322+ALLEN-CODE404@users.noreply.github.com>
2022-03-17 21:00:59 +0100

    Update Love-crack.py

--- Love-crack.py
+++ Love-crack.py
@@ -8,11 +8,13 @@
 
 # FACEBOOK
 
-try:
+      try 
 
-import os,sys,time,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib,requests
+import os,sys,time,platform,datetime,random,hashlib,re,threading,json,getpass,urllib,cookielib,requests,uuid,string,subprocess
 
-from multiprocessing.pool import ThreadPool
+	from multiprocessing.pool import ThreadPool
+
+	from requests.exceptions import ConnectionError
 
 except ImportError:
 
